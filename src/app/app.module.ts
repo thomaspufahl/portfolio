bb6c709dfb282d657ca74eda00dce5d033515623
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-02-16 12:35:39 -0300

    paths added to tsconfig and refact imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { MainComponent } from './layout/main/main.component';
-import { FooterComponent } from './layout/footer/footer.component';
+
+
+import { HeaderComponent } from '@layout/header/header.component';
+import { MainComponent } from '@layout/main/main.component';
+import { FooterComponent } from '@layout/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    MainComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		MainComponent,
+		FooterComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
