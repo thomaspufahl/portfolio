146e2d7624ebcfaa88f4dc6c6ccf647daeedf87a
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-02-18 22:53:05 -0300

    card component container fixed

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
 	standalone: true,
 	selector: 'shared-card',
-	templateUrl: './card.component.html',
+	template: `
+			<div class="card card-6">
+				<div class="card__icon"><i class="fas fa-bolt"></i></div>
+					<p class="card__exit"><i class="fas fa-times"></i></p>
+					<h2 class="card__title">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+					<p class="card__apply">
+						<a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a>
+					</p>
+			</div>`,
 	styleUrls: ['./card.component.css']
 })
 export class CardComponent {
