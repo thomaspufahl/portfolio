c1400887809e000222f79b86aa37aab30caf75b7
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-02-19 20:29:21 -0300

    edit-button component added

---
+++ src/app/shared/components/edit-button/edit-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	standalone: true,
+	selector: 'edit-button',
+	templateUrl: './edit-button.component.html',
+	styleUrls: ['./edit-button.component.css']
+})
+export class EditButtonComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { CardComponent } from './components/card/card.component';
 import { TitleComponent } from './components/title/title.component';
+import { EditButtonComponent } from './components/edit-button/edit-button.component';
 
 
 
 @NgModule({
 	declarations: [
-	],
+	
+    EditButtonComponent
+  ],
 	imports: [
 		CommonModule,
 		CardComponent,
