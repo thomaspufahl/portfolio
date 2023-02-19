786bb94afc4001fa88744f824fdc1cfebe177b89
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-02-18 21:55:59 -0300

    projects component added

--- src/app/modules/visitor/visitor.module.ts
+++ src/app/modules/visitor/visitor.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { VisitorRoutingModule } from './visitor-routing.module';
 import { VisitorComponent } from './pages/visitor/visitor.component';
 import { TitleComponent } from './components/title/title.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 
 @NgModule({
 	declarations: [
 		VisitorComponent,
 		TitleComponent,
+		ProjectsComponent,
 	],
 	imports: [
 		CommonModule,
